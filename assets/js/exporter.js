ed7e1fd06f9acae0244e59ec43e0eec66c295172
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-01-22 12:25:15 +0530

    Update exporter.js

--- assets/js/exporter.js
+++ assets/js/exporter.js
@@ -1,1 +1,8 @@
-
+function exportCSV(data, name) {
+const csv = Papa.unparse(data);
+const blob = new Blob([csv]);
+const a = document.createElement('a');
+a.href = URL.createObjectURL(blob);
+a.download = name;
+a.click();
+}
