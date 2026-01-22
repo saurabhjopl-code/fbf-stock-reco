d6e832ebf6d4cfb7fa3f2d4085cb726c09366615
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-01-22 12:25:03 +0530

    Update uiRenderer.js

--- assets/js/uiRenderer.js
+++ assets/js/uiRenderer.js
@@ -1,1 +1,12 @@
-
+function renderTable(container, data) {
+let html = '<table><tr>';
+Object.keys(data[0] || {}).forEach(h => html += `<th>${h}</th>`);
+html += '</tr>';
+data.forEach(r => {
+html += '<tr>';
+Object.values(r).forEach(v => html += `<td>${v}</td>`);
+html += '</tr>';
+});
+html += '</table>';
+container.innerHTML = html;
+}
