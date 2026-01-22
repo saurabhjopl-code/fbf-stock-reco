18be070037640a8280b49e4601f01f81ae2c3da4
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-01-22 12:35:20 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,12 @@
 document.getElementById('generateBtn').onclick = async () => {
-const sales = await parseFile(document.getElementById('saleFile').files[0]);
-const fbf = await parseFile(document.getElementById('fbfFile').files[0]);
-const uni = await parseFile(document.getElementById('uniFile').files[0]);
-const fcAsk = await parseFile(document.getElementById('fcRecoFile').files[0]);
+const s = await parse(saleFile.files[0]);
+const f = await parse(fbfFile.files[0]);
+const u = await parse(uniFile.files[0]);
+const a = await parse(fcAskFile.files[0]);
 
 
-const consolidated = consolidateData(sales, fbf, uni, fcAsk);
-document.getElementById('progressBar').style.width = '100%';
-
-
-renderTable(document.getElementById('fcSummary'), consolidated.sales.slice(0,5));
+const res = runEngine(s,f,u,a);
+renderTabs(res);
 document.getElementById('exportAllBtn').disabled = false;
+document.getElementById('exportAllBtn').onclick = ()=>exportAll(res);
 };
