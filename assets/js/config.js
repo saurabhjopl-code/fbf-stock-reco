71deaa1faf7e0b95214962c69a0c3a4a9dea8149
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-01-22 12:23:48 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,1 +1,5 @@
-
+window.APP_CONFIG = {
+get targetSC() { return +document.getElementById('cfgTargetSC').value; },
+get minUniware() { return +document.getElementById('cfgMinUni').value; },
+get maxReturn() { return +document.getElementById('cfgMaxReturn').value; }
+};
